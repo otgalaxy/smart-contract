43188459cdf33c821a92cbfe38b8370ddbfb5daa
WEB5LAB <88079157+web5lab@users.noreply.github.com>
2024-02-15 16:53:55 +0530

    Update staking.sol

--- staking.sol
+++ staking.sol
@@ -1,1 +1,178 @@
+// SPDX-License-Identifier: MIT
+/**
+  ______   .___________.  _______      ___       __          ___      ___   ___ ____    ____ 
+ /  __  \  |           | /  _____|    /   \     |  |        /   \     \  \ /  / \   \  /   / 
+|  |  |  | `---|  |----`|  |  __     /  ^  \    |  |       /  ^  \     \  V  /   \   \/   /  
+|  |  |  |     |  |     |  | |_ |   /  /_\  \   |  |      /  /_\  \     >   <     \_    _/   
+|  `--'  |     |  |     |  |__| |  /  _____  \  |  `----./  _____  \   /  .  \      |  |     
+ \______/      |__|      \______| /__/     \__\ |_______/__/     \__\ /__/ \__\     |__|                                                                                                
+**/
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract OtGalaxyStaking is Context, ReentrancyGuard {
+    IERC20 public stakingToken;
+    IERC20 public rewardsToken;
+
+    address public owner;
+
+    // Duration of rewards to be paid out (in seconds)
+    uint256 public duration;
+    // Timestamp of when the rewards finish
+    uint256 public finishAt;
+    // Minimum of last updated time and reward finish time
+    uint256 public updatedAt;
+    // Reward to be paid out per second
+    uint256 public rewardRate;
+    // Sum of (reward rate * dt * 1e18 / total supply)
+    uint256 public rewardPerTokenStored;
+    // User address => rewardPerTokenStored
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    // User address => rewards to be claimed
+    mapping(address => uint256) public rewards;
+
+    // Total staked
+    uint256 public totalSupply;
+    // User address => staked amount
+    mapping(address => uint256) public balanceOf;
+
+    constructor(address _stakingToken, address _rewardToken) {
+        owner = msg.sender;
+        stakingToken = IERC20(_stakingToken);
+        rewardsToken = IERC20(_rewardToken);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not authorized");
+        _;
+    }
+
+    modifier updateReward(address _account) {
+        rewardPerTokenStored = rewardPerToken();
+        updatedAt = lastTimeRewardApplicable();
+
+        if (_account != address(0)) {
+            rewards[_account] = earned(_account);
+            userRewardPerTokenPaid[_account] = rewardPerTokenStored;
+        }
+
+        _;
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return _min(finishAt, block.timestamp);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+
+        return
+            rewardPerTokenStored +
+            (rewardRate * (lastTimeRewardApplicable() - updatedAt) * 1e18) /
+            totalSupply;
+    }
+
+    function stake(uint256 _amount)
+        external
+        nonReentrant
+        updateReward(msg.sender)
+    {
+        require(_amount > 0, "amount = 0");
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+        balanceOf[msg.sender] += _amount;
+        totalSupply += _amount;
+    }
+
+    function withdraw(uint256 _amount)
+        external
+        nonReentrant
+        updateReward(msg.sender)
+    {
+        require(_amount > 0, "amount = 0");
+        balanceOf[msg.sender] -= _amount;
+        totalSupply -= _amount;
+        stakingToken.transfer(msg.sender, _amount);
+    }
+
+    function earned(address _account) public view returns (uint256) {
+        return
+            ((balanceOf[_account] *
+                (rewardPerToken() - userRewardPerTokenPaid[_account])) / 1e18) +
+            rewards[_account];
+    }
+
+    function getReward() external nonReentrant updateReward(msg.sender) {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardsToken.transfer(msg.sender, reward);
+        }
+    }
+
+    function setRewardsDuration(uint256 _duration) external onlyOwner {
+        require(finishAt < block.timestamp, "reward duration not finished");
+        duration = _duration;
+    }
+
+    function notifyRewardAmount(uint256 _amount)
+        external
+        onlyOwner
+        updateReward(address(0))
+    {
+        if (block.timestamp >= finishAt) {
+            rewardRate = _amount / duration;
+        } else {
+            uint256 remainingRewards = (finishAt - block.timestamp) *
+                rewardRate;
+            rewardRate = (_amount + remainingRewards) / duration;
+        }
+
+        require(rewardRate > 0, "reward rate = 0");
+        require(
+            rewardRate * duration <= rewardsToken.balanceOf(address(this)),
+            "reward amount > balance"
+        );
+
+        finishAt = block.timestamp + duration;
+        updatedAt = block.timestamp;
+    }
+
+    function addRewardTokens(uint256 _amount)
+        external
+        onlyOwner
+        updateReward(address(0))
+    {
+        rewardsToken.transferFrom(msg.sender, address(this), _amount);
+
+        if (block.timestamp >= finishAt) {
+            // If the reward duration has already finished, set a new reward rate
+            rewardRate = _amount / duration;
+        } else {
+            // If the reward duration is ongoing, calculate the remaining rewards
+            uint256 remainingRewards = (finishAt - block.timestamp) *
+                rewardRate;
+            rewardRate = (_amount + remainingRewards) / duration;
+        }
+
+        require(rewardRate > 0, "reward rate = 0");
+        require(
+            rewardRate * duration <= rewardsToken.balanceOf(address(this)),
+            "reward amount > balance"
+        );
+
+        // Update the finishAt timestamp to reflect the extended reward duration
+        finishAt = block.timestamp + duration;
+        updatedAt = block.timestamp;
+    }
+
+    function _min(uint256 x, uint256 y) private pure returns (uint256) {
+        return x <= y ? x : y;
+    }
+}
 
